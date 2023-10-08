84c65f6c970b5879982856da81bff6468444b468
Tim-Siu <e1088140@u.nus.edu>
2023-10-08 16:07:29 +0800

    Add Description class to Person model with tests
    - Introduced Description class to enhance the Person model.
    - Prepared the foundation to achieve the following:
      * Store all People and Companies in PeopleList and CompaniesList respectively.
      * Maintain a filtered ObservableList<People> for selected People objects (e.g., search results) for UI binding.
      * Maintain a sorted and filtered ObservableList<Companies> for selected Companies, allowing UI to auto-update based on changes.
      * Incorporate a UserPref object to capture user preferences, exposed as ReadOnlyUserPref.
      * Ensure Model remains independent of other components for integrity.
    
    Further refinements will be needed to fully realize the described features.

---
+++ src/test/java/seedu/address/model/person/DescriptionTest.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class DescriptionTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Description(null));
+    }
+
+    @Test
+    public void constructor_invalidDescription_throwsIllegalArgumentException() {
+        String invalidDescription = "";
+        assertThrows(IllegalArgumentException.class, () -> new Description(invalidDescription));
+    }
+
+    @Test
+    public void isValidDescription() {
+        // null description
+        assertThrows(NullPointerException.class, () -> Description.isValidDescription(null));
+
+        // invalid descriptions
+        assertFalse(Description.isValidDescription("")); // empty string
+        assertFalse(Description.isValidDescription(" ")); // spaces only
+
+        // valid descriptions
+        assertTrue(Description.isValidDescription("This is a valid description."));
+        assertTrue(Description.isValidDescription("Short")); // short description
+        assertTrue(Description.isValidDescription("Longer descriptions should also be perfectly valid to handle."));
+    }
+
+    @Test
+    public void equals() {
+        Description description = new Description("Sample description");
+
+        // same values -> returns true
+        assertTrue(description.equals(new Description("Sample description")));
+
+        // same object -> returns true
+        assertTrue(description.equals(description));
+
+        // null -> returns false
+        assertFalse(description.equals(null));
+
+        // different types -> returns false
+        assertFalse(description.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(description.equals(new Description("Different description")));
+    }
+}
