c22ef34cf2e4f0543600376c3b7e5dd1ffc4d2d1
oeggy03 <oeggy03>
2023-10-12 15:54:50 +0800

    Update List Command
    Added a ListCommandParser so that it accomodates the list p/c arguments

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,8 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 
 /**
@@ -12,13 +14,34 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String LIST_COMPANIES_ARG_WORD = "c";
 
+    public static final String LIST_PERSONS_ARG_WORD = "p";
 
+    public String argument;
+
+    public static final String MESSAGE_SUCCESS_PERSONS = "Listed all persons";
+
+    public static final String MESSAGE_SUCCESS_COMPANIES = "Listed all companies";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all persons or companies. \n"
+            + "Parameters: \n"
+            + "c - Lists all companies\n"
+            + "p - Lists all persons \n"
+            + "Example: " + COMMAND_WORD + " p";
+
+    public ListCommand(String arg) {
+        this.argument = arg;
+    }
+
+    public CommandResult executeListPersons(Model model) {
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS_PERSONS);
+    }
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+
+        return executeListPersons(model);
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class ListCommandParser implements Parser<ListCommand> {
+
+    /**
+     * Used to confirm if the arguments are either c or p for list command.
+     */
+    private static final Pattern ARGUMENT_REGEX_PATTERN = Pattern.compile("^(c|p)$");
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ListCommand
+     * and returns a ListCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ListCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+
+        // Used to check if argument is either c or p
+        Matcher matcher = ARGUMENT_REGEX_PATTERN.matcher(trimmedArgs);
+
+        // Throw an error, if argument is invalid or empty
+        if (trimmedArgs.isEmpty() || !matcher.matches()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        return new ListCommand(trimmedArgs);
+    }
+
+}
