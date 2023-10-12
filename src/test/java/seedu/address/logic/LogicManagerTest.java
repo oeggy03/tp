34363ed502850b0b62f42c249f479bc1fcced8ac
oeggy03 <oeggy03>
2023-10-12 16:49:13 +0800

    Update tests for the list p command
    Renamed ListCommandTest to ListPersonsCommandTest to improve abstraction.
    Update parseCommand_list() in AddressBookParserTest to accomodate the new format of the list command (list p and list c).

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.io.TempDir;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListPersonsCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
@@ -66,8 +67,8 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() throws Exception {
-        String listCommand = ListCommand.COMMAND_WORD;
-        assertCommandSuccess(listCommand, ListCommand.MESSAGE_SUCCESS, model);
+        String listCommand = ListCommand.COMMAND_WORD + " p";
+        assertCommandSuccess(listCommand, ListPersonsCommand.MESSAGE_SUCCESS, model);
     }
 
     @Test
--- src/test/java/seedu/address/logic/commands/ListPersonsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListPersonsCommandTest.java
@@ -15,7 +15,7 @@ import seedu.address.model.UserPrefs;
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
  */
-public class ListPersonCommandTest {
+public class ListPersonsCommandTest {
 
     private Model model;
     private Model expectedModel;
@@ -28,12 +28,12 @@ public class ListPersonCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListPersonsCommand("p"), model, ListPersonsCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListPersonsCommand("p"), model, ListPersonsCommand.MESSAGE_SUCCESS, expectedModel);
     }
 }
