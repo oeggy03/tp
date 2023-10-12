f6a5fbc36a9557f34252a7d5bce4bb4d0ccc2726
lunaroddity <e0969600@u.nus.edu>
2023-10-13 04:49:44 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/model/person/ContactIsEqualsPredicateTest.java
+++ src/test/java/seedu/address/model/person/ContactIsEqualsPredicateTest.java
@@ -1,12 +1,7 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
