a6b3a69b3f68df42de6ee5452354fadf7dd869be
Tim-Siu <e1088140@u.nus.edu>
2023-10-14 18:07:05 +0800

    Enhance AddressBook: Introduce Company Model
    - Incorporated Company Model to extend AddressBook's capabilities.
    - Completed tests for most company-related classes; `UniqueCompanyListTest` remains.
    - Noted: 'person' model requires further refinement.
    - Initiated groundwork to adjust storage for company data.
    
    In this commit, the AddressBook, originally designed to store personal details, now begins its transition to also support company information.

---
+++ src/main/java/seedu/address/model/company/Company.java
@@ -0,0 +1,116 @@
+package seedu.address.model.company;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents a Company in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Company {
+
+    // Identity fields
+    private final CompanyName companyName;
+    private final Phone phone;
+    private final Email email;
+
+    // Data fields
+    private final Description description;
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Company(CompanyName companyName, Phone phone, Email email, Description description, Set<Tag> tags) {
+        requireAllNonNull(companyName, phone, email, description, tags);
+        this.companyName = companyName;
+        this.phone = phone;
+        this.email = email;
+        this.description = description;
+        this.tags.addAll(tags);
+    }
+
+    public CompanyName getName() {
+        return companyName;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Description getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both companys have the same name.
+     * This defines a weaker notion of equality between two companys.
+     */
+    public boolean isSameCompany(Company otherCompany) {
+        if (otherCompany == this) {
+            return true;
+        }
+
+        return otherCompany != null
+            && otherCompany.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both companys have the same identity and data fields.
+     * This defines a stronger notion of equality between two companys.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Company)) {
+            return false;
+        }
+
+        Company otherCompany = (Company) other;
+        return companyName.equals(otherCompany.companyName)
+            && phone.equals(otherCompany.phone)
+            && email.equals(otherCompany.email)
+            && description.equals(otherCompany.description)
+            && tags.equals(otherCompany.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(companyName, phone, email, description, tags);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .add("name", companyName)
+            .add("phone", phone)
+            .add("email", email)
+            .add("description", description)
+            .add("tags", tags)
+            .toString();
+    }
+}
---
+++ src/test/java/seedu/address/testutil/CompanyBuilder.java
@@ -0,0 +1,95 @@
+package seedu.address.testutil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.company.Company;
+import seedu.address.model.company.CompanyName;
+import seedu.address.model.company.Description;
+import seedu.address.model.company.Email;
+import seedu.address.model.company.Phone;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+
+/**
+ * A utility class to help with building Company objects.
+ */
+public class CompanyBuilder {
+
+    public static final String DEFAULT_NAME = "TechCorp Inc.";
+    public static final String DEFAULT_PHONE = "12345678";
+    public static final String DEFAULT_EMAIL = "contact@techcorp.com";
+    public static final String DEFAULT_DESCRIPTION = "A leading tech company.";
+
+    private CompanyName name;
+    private Phone phone;
+    private Email email;
+    private Description description;
+    private Set<Tag> tags;
+
+    /**
+     * Creates a {@code CompanyBuilder} with the default details.
+     */
+    public CompanyBuilder() {
+        name = new CompanyName(DEFAULT_NAME);
+        phone = new Phone(DEFAULT_PHONE);
+        email = new Email(DEFAULT_EMAIL);
+        description = new Description(DEFAULT_DESCRIPTION);
+        tags = new HashSet<>();
+    }
+
+    /**
+     * Initializes the CompanyBuilder with the data of {@code companyToCopy}.
+     */
+    public CompanyBuilder(Company companyToCopy) {
+        name = companyToCopy.getName();
+        phone = companyToCopy.getPhone();
+        email = companyToCopy.getEmail();
+        description = companyToCopy.getDescription();
+        tags = new HashSet<>(companyToCopy.getTags());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Company} that we are building.
+     */
+    public CompanyBuilder withName(String name) {
+        this.name = new CompanyName(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Company} that we are building.
+     */
+    public CompanyBuilder withTags(String... tags) {
+        this.tags = SampleDataUtil.getTagSet(tags);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code Company} that we are building.
+     */
+    public CompanyBuilder withDescription(String description) {
+        this.description = new Description(description);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Phone} of the {@code Company} that we are building.
+     */
+    public CompanyBuilder withPhone(String phone) {
+        this.phone = new Phone(phone);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Email} of the {@code Company} that we are building.
+     */
+    public CompanyBuilder withEmail(String email) {
+        this.email = new Email(email);
+        return this;
+    }
+
+    public Company build() {
+        return new Company(name, phone, email, description, tags);
+    }
+}
