bb3b92294cd754830e093302e78d094c7ba1ea8d
Rongzhi Li <e1088149@u.nus.edu>
2023-10-31 15:15:21 +0800

    Add tests for ClearInternshipCommand

--- src/test/java/seedu/address/logic/commands/clearcommands/ClearInternshipCommandTest.java
+++ src/test/java/seedu/address/logic/commands/clearcommands/ClearInternshipCommandTest.java
@@ -1,18 +1,25 @@
 package seedu.address.logic.commands.clearcommands;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.testutil.TypicalCompanies.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON_OR_COMPANY;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.CommandTestUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.company.Company;
+import seedu.address.model.company.internship.Internship;
 import seedu.address.testutil.CompanyBuilder;
-
 
 public class ClearInternshipCommandTest {
     private Model model;
@@ -20,19 +27,37 @@ public class ClearInternshipCommandTest {
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        Company validCompany = new CompanyBuilder().build();
-        model.addCompany(validCompany);
     }
 
     @Test
-    public void execute_noInternshipCompany_success() {
+    public void execute_clearInternshipCompany_success() {
         ClearInternshipCommand clearInternshipCommand = new ClearInternshipCommand(INDEX_FIRST_PERSON_OR_COMPANY);
 
-        String expectedMessage = String.format(
-                ClearInternshipCommand.MESSAGE_SUCCESS, INDEX_FIRST_PERSON_OR_COMPANY);
-        Model expectedModel = model;
-        CommandTestUtil.assertRegularCommandSuccess(clearInternshipCommand, model, expectedMessage, expectedModel);
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        List<Company> lastShownList = expectedModel.getFilteredCompanyList();
+        Company targetCompany = lastShownList.get(INDEX_FIRST_PERSON_OR_COMPANY.getZeroBased());
+        Set<Internship> internshipsToKeep = new HashSet<>();
+        Company updatedCompany = new CompanyBuilder(targetCompany).withInternships(internshipsToKeep).build();
+        expectedModel.setCompany(targetCompany, updatedCompany);
 
+        CommandTestUtil.assertRegularCommandSuccess(clearInternshipCommand, model,
+                String.format(clearInternshipCommand.MESSAGE_SUCCESS, INDEX_FIRST_PERSON_OR_COMPANY), expectedModel
+        );
     }
 
+    @Test
+    public void execute_clearInternshipCompanyOutOfIndexScope_failure() {
+        ClearInternshipCommand clearInternshipCommand = new ClearInternshipCommand(
+                Index.fromZeroBased(100)
+        );
+        CommandTestUtil.assertInternshipCommandFailure(
+                clearInternshipCommand, model,
+                Messages.MESSAGE_INVALID_COMPANY_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_clearInternshipWithNullIndex_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ClearInternshipCommand(
+                null).execute(model));
+    }
 }
