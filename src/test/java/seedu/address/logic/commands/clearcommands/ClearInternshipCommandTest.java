31b9b6eeab6a553d80b35af00d121983044324b3
Rongzhi Li <e1088149@u.nus.edu>
2023-10-31 12:32:07 +0800

    Add testing for ClearInternshipCommand

--- src/test/java/seedu/address/logic/commands/clearcommands/ClearInternshipCommandTest.java
+++ src/test/java/seedu/address/logic/commands/clearcommands/ClearInternshipCommandTest.java
@@ -1,15 +1,21 @@
 package seedu.address.logic.commands.clearcommands;
 
 import static seedu.address.testutil.TypicalCompanies.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON_OR_COMPANY;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.CommandTestUtil;
+import seedu.address.logic.commands.editcommand.EditCompanyCommand;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.company.Company;
 import seedu.address.testutil.CompanyBuilder;
-
+import seedu.address.testutil.EditCompanyDescriptorBuilder;
 
 
 public class ClearInternshipCommandTest {
@@ -20,6 +26,17 @@ public class ClearInternshipCommandTest {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         Company validCompany = new CompanyBuilder().build();
         model.addCompany(validCompany);
+    }
+
+    @Test
+    public void execute_NoInternshipCompany_Success() {
+        ClearInternshipCommand clearInternshipCommand = new ClearInternshipCommand(INDEX_FIRST_PERSON_OR_COMPANY);
+
+        String expectedMessage = String.format(
+                ClearInternshipCommand.MESSAGE_SUCCESS, INDEX_FIRST_PERSON_OR_COMPANY);
+        Model expectedModel = model;
+        CommandTestUtil.assertRegularCommandSuccess(clearInternshipCommand, model, expectedMessage, expectedModel);
+
     }
 
 }
